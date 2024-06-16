3fff69e8c25ee86f93af1f895bb51416cb1a1118
Valters <103326282+Raigodo@users.noreply.github.com>
2024-06-16 21:44:50 +0300

    Journal feature draft

---
+++ Journal/Features/Person/Create/CreateNoteRequest.cs
@@ -0,0 +1,16 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Journal.Features.Notes.Create;
+
+public record CreateNoteRequest(string Input)
+{
+    public sealed class RequestValidator : Validator<CreateNoteRequest>
+    {
+        public RequestValidator()
+        {
+            RuleFor(x => x.Input)
+                .NotNull().NotEmpty().WithMessage("Input not specified! >:(");
+        }
+    }
+}
---
+++ Journal/Features/Person/Create/Endpoint.cs
@@ -0,0 +1,20 @@
+﻿using FastEndpoints;
+using Journal.Data;
+
+namespace Journal.Features.Notes.Create;
+
+public sealed class Endpoint : Endpoint<CreateNoteRequest>
+{
+    public override void Configure()
+    {
+        Post("notes");
+        AllowAnonymous();
+    }
+
+    public JournalDbContext Db { get; set; }
+
+    public override async Task HandleAsync(CreateNoteRequest r, CancellationToken c)
+    {
+        await SendAsync(new { Message = "Hi" }, cancellation: c);
+    }
+}
